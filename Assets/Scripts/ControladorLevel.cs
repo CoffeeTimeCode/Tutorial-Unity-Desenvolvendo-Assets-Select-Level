3699f52be9030ac362182b396fb3b190f87e2b48
jsnunes <jesiel.nunes@hotmail.com>
2017-10-01 19:22:23 -0300

    #3

---
+++ Assets/Scripts/ControladorLevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorLevel : MonoBehaviour {
+
+	public int _pontos;
+	public Level _nextLevel;
+	public ControladorJogador _ctlJogador;
+
+	// Use this for initialization
+	void Start () {
+		_ctlJogador = GameObject.FindObjectOfType<ControladorJogador> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void adicionarPontos(int pontos){
+		_pontos += pontos;
+		for (int i = 0; i < _ctlJogador._jogador.levels.Count; i++) {
+			if(_ctlJogador._jogador.levels[i].level==(_nextLevel.level-1)){
+				_ctlJogador._jogador.levels [i].pontos = _pontos;
+				if(_ctlJogador._jogador.levels[i].pontos>=_nextLevel.pontos&&_ctlJogador._jogador.levels.Count==(_nextLevel.level-1)){
+					_ctlJogador._jogador.levels.Add (new Level(_nextLevel.level,0));
+				}
+			}
+		}
+	}
+
+	public void votlarMenu(){
+		Destroy (_ctlJogador.gameObject);
+		SceneManager.LoadScene ("Menu");
+	}
+}
--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Jogador {
 	public string nome;
 	public List<Level> levels;
